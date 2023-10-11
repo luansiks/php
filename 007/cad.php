e7ff1be2a4ba8c09dcffe7c33851d3fa16358b7c
luansiks <luansikssantos10@gmail.com>
2023-10-11 11:02:55 -0300

    up

---
+++ 007/cad.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>resultado</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header><h1>resultado do processamento</h1></header>
+    <main>
+        <?php 
+            $nome = $_REQUEST["nome"] ?? "desconecido";
+            $sobrenome = $_REQUEST["sobrenome"] ?? "não indentificado";
+            print"é um prazer te conhecer <strong>$nome $sobrenome</strong>.";
+        ?>
+        <p><a href="index.html" target="_self" real="exit">sair</a></p>
+    </main>
+</body>
+</html>
